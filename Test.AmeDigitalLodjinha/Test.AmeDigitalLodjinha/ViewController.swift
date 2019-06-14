54675144d4204127111ec6c96e1e6625d9512371
Leandro Romano <leandroromano.dev@gmail.com>
2019-06-14 11:53:50 -0300

    Project created for Ame Digital's test.

---
+++ Test.AmeDigitalLodjinha/Test.AmeDigitalLodjinha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test.AmeDigitalLodjinha
+//
+//  Created by Leandro Romano on 14/06/19.
+//  Copyright © 2019 Leandro Romano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello World, Ame Digital! 😁")
+    }
+
+
+}
+
