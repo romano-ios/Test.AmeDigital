738509917615019c21e75fe93fb4e122367c22be
Leandro Romano <leandroromano.dev@gmail.com>
2019-06-15 00:20:13 -0300

    Endpoint provider for Categories.

--- Test.AmeDigitalLodjinha/Test.AmeDigitalLodjinha/Networking/Endpoint Providers/Endpoint+Categories.swift
+++ Test.AmeDigitalLodjinha/Test.AmeDigitalLodjinha/Networking/Endpoint Providers/Endpoint+Categories.swift
@@ -1,16 +1,18 @@
 //
-//  Endpoint+Banner.swift
+//  Endpoint+Categories.swift
 //  Test.AmeDigitalLodjinha
 //
 //  Created by Leandro Romano on 15/06/19.
 //  Copyright © 2019 Leandro Romano. All rights reserved.
 //
 
+import Foundation
+
 extension Endpoint {
     
-    static func getBanners() -> Endpoint {
+    static func getCategories() -> Endpoint {
         return Endpoint(
-            path: Constants.apiBannerUrl,
+            path: Constants.apiCategoryUrl,
             method: .get,
             queryItems: []
         )
