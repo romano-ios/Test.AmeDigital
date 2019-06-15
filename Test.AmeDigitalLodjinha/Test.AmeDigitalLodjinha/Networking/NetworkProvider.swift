b605fe75be1afc249e4dd28bd505209674d31fb0
Leandro Romano <leandroromano.dev@gmail.com>
2019-06-15 00:02:37 -0300

    Implementation for generic NetworkProvider.

--- Test.AmeDigitalLodjinha/Test.AmeDigitalLodjinha/Networking/NetworkProvider.swift
+++ Test.AmeDigitalLodjinha/Test.AmeDigitalLodjinha/Networking/NetworkProvider.swift
@@ -7,11 +7,29 @@
 //
 
 import Foundation
+import PromiseKit
 
 class NetworkProvider {
     
     private init() { }
     
     static let shared: NetworkProvider = NetworkProvider()
+    
+    func request<T: Decodable>(_ endpoint: Endpoint) -> Promise<T> {
+        return Promise { seal in
+            guard let url = endpoint.url else { return seal.reject(NetworkError.badUrl) }
+            
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                guard let data = data else { return seal.reject(NetworkError.emptyResponseDataError) }
+                
+                do {
+                    let decodableObject = try JSONDecoder().decode(T.self, from: data)
+                    return seal.fulfill(decodableObject)
+                } catch {
+                    return seal.reject(NetworkError.mappingError)
+                }
+            }.resume()
+        }
+    }
     
 }
